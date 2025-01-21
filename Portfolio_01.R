a87d8f6d0d687a4804d674a3afd86f14a38ec9d5
graceeh146 <enhuiwang146@gmail.com>
2025-01-21 17:07:22 -0500

    Done 1

---
+++ Portfolio_01.R
@@ -0,0 +1,12 @@
+  ---
+  title: "Portfolio 1"
+  author: "Enhui Wang"
+  date: "01.21.2025"
+  output: github_document
+---
+  #Load packages and data
+  library(dplyr)
+  library(ggplot2)
+  Data_01 <- read_csv("Data_01.csv")
+
+
